afd551c27355099fe718751523c6b4d8d2ab8cce
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-05-06 10:40:09 -0300

    feat: Dependencies inversions

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import type { FastifyRequest, FastifyReply } from 'fastify'
 import z from 'zod'
-import { registerUseCase } from '@/use-cases/register.js'
+import { RegisterUseCase } from '@/use-cases/register.js'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository.js'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,7 +13,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    await registerUseCase({
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    await registerUseCase.execute({
       name,
       email,
       password,
